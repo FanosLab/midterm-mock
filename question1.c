375d1631d927b61823b0aa3f3a7d834d95270469
FanosLab <53920867+FanosLab@users.noreply.github.com>
2021-10-26 06:41:11 -0400

    Create question1.c

---
+++ question1.c
@@ -0,0 +1,19 @@
+/*In this question you are required to find whether a character exists in a string
+(a) [XX Marks] write the function int char_exists(char target, char str[]) that returns 1 if target exists in str and 0 otherwise.
+(b) [Y MArks] complete the main function to test your function (by simply calling it and check the result)*/
+
+
+int char_exists(char target, char str[]){
+  
+  
+  
+  
+}
+
+int main(){
+  
+  
+  
+  
+  return 0;
+}
