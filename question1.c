d71842bc801851c28baf8c510ea40716953d6d6c
FanosLab <53920867+FanosLab@users.noreply.github.com>
2021-10-26 06:58:51 -0400

    Update question1.c

--- question1.c
+++ question1.c
@@ -5,13 +5,7 @@
 
 int char_exists(char target, char str[]){
   
-  for (int i=0;str[i]!='\0';i++){
-    
-    if(str[i]==target){
-      return 1;
-    }
-    
-  }
+  
   
   return 0;
   
@@ -20,19 +14,7 @@ int char_exists(char target, char str[]){
 
 int main(){
   
-  /*test case found*/
-  char input_str[10]="COMPENG2SH4";
-  char input_char ='E';
-  int expected =1;
-  int actual = char_exists(input_char,input_str);
-  
-  if (actual==expected){
-    printf("Executed correctly\n");
-  }
-  else {
-    printf("ERROR: expected is: %d, while found is:%d\n",expected,found);
-  }
-  
+ 
   
   return 0;
 }
